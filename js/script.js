c0b55800fb25b6feebe0c560156a8a8b33f8b188
Alan <alanzagi2007@gmail.com>
2024-08-24 17:12:22 +0700

    menyelesaikan modal tiap grid

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.querySelectorAll(".hubungi").forEach(function (button) {
+  button.addEventListener("click", function () {
+    window.location.href = "https://wa.me/6281521550913?text=halo";
+  });
+});
