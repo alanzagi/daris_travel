57ea0c55e85c72e0d4abc8234847cc64bce1e079
Alan <alanzagi2007@gmail.com>
2024-08-25 16:50:57 +0700

    memperbaharui tampilan index.html

--- js/script.js
+++ js/script.js
@@ -1,3 +1,41 @@
+// Start Fungsi untuk penambahan angka
+document.addEventListener("DOMContentLoaded", function () {
+  const counters = document.querySelectorAll(".counter");
+
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const counter = entry.target;
+          const target = +counter.getAttribute("data-target");
+          const duration = 2000;
+          const start = 0;
+          let startTime = null;
+
+          const updateCounter = (currentTime) => {
+            if (!startTime) startTime = currentTime;
+            const elapsedTime = currentTime - startTime;
+            const progress = Math.min(elapsedTime / duration, 1);
+            counter.textContent = Math.floor(progress * target) + "+";
+            if (progress < 1) {
+              requestAnimationFrame(updateCounter);
+            } else {
+              counter.textContent = target + "+";
+            }
+          };
+
+          requestAnimationFrame(updateCounter);
+          observer.unobserve(counter);
+        }
+      });
+    },
+    { threshold: 0.1 }
+  );
+
+  counters.forEach((counter) => observer.observe(counter));
+});
+// End Fungsi untuk penambahan angka
+
 document.querySelectorAll(".hubungi").forEach(function (button) {
   button.addEventListener("click", function () {
     window.location.href = "https://wa.me/6281521550913?text=halo";
